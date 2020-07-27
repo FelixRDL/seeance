7efa15465c0befd98d3fea40040c54f12d9e6ead
FelixRDL <felix1.riedl@stud.uni-regensburg.de>
2020-07-27 09:35:19 +0200

    Add ids to git

--- cogito-backend/src/implementation/controllers/StudyController.ts
+++ cogito-backend/src/implementation/controllers/StudyController.ts
@@ -9,6 +9,17 @@ import {CreateProject, CreateProjectRequest} from "../../logic/use-cases/project
 import {AddProjectToCourseById} from "../../logic/use-cases/courses/AddProjectToCourseById";
 import {InternalRepositoryProvider} from "../providers/InternalRepositoryProvider";
 import {InternalProjectRepository} from "../providers/InternalProjectRepository";
+import {AddPreprocessorToProject} from "../../logic/use-cases/projects/AddPreprocessorToProject";
+import {CreatePreprocessor} from "../../logic/use-cases/preprocessors/CreatePreprocessor";
+import {PreprocessorTemplateRepository} from "../../logic/repositories/analysis/PreprocessorTemplateRepository";
+import {InternalComponentTemplateProviderAccess} from "../providers/InternalComponentTemplateProvider";
+import {PreprocessorRepository} from "../../logic/repositories/analysis/PreprocessorRepository";
+import {InternalPreprocessorRepository} from "../providers/InternalPreprocessorRepository";
+import {CreateAnalysis} from "../../logic/use-cases/analyses/CreateAnalysis";
+import {AddAnalysisToProject} from "../../logic/use-cases/projects/AddAnalysisToProject";
+import {AnalysisTemplateRepository} from "../../logic/repositories/analysis/AnalysisTemplateRepository";
+import {AnalysisRepository} from "../../logic/repositories/analysis/AnalysisRepository";
+import {InternalAnalysisProvider} from "../providers/InternalAnalysisProvider";
 
 
 export class StudyController {
@@ -19,7 +30,41 @@ export class StudyController {
         return createHash('md5').update(login).digest('hex')
     }
 
-    async addProject(courseId: string, projectId: string) {
+    private async addAnalysis(courseId: string, projectId: string, analysisName: string) {
+        const analysisTemplateRepository: AnalysisTemplateRepository = InternalComponentTemplateProviderAccess.getInstance();
+        const analysisRepository: AnalysisRepository = new InternalAnalysisProvider();
+        const analysis = await CreateAnalysis({
+                courseId: courseId,
+                projectId: projectId,
+                name: analysisName,
+                template: analysisName
+            }, analysisRepository,
+            analysisTemplateRepository)
+        const newAnalyses: string[] = await AddAnalysisToProject({
+            analysisId: analysis._id,
+            courseId: courseId,
+            projectId: projectId
+        }, new InternalProjectRepository())
+    }
+
+    private async addPreprocessor(courseId: string, projectId: string, templateName: string) {
+        const preprocessorTemplateRepository: PreprocessorTemplateRepository = InternalComponentTemplateProviderAccess.getInstance();
+        const preprocessorRepository: PreprocessorRepository = new InternalPreprocessorRepository();
+        const preprocessor = await CreatePreprocessor({
+                courseId: courseId,
+                projectId: projectId,
+                name: templateName,
+                template: templateName
+            }, preprocessorRepository,
+            preprocessorTemplateRepository)
+        await AddPreprocessorToProject({
+            preprocessorId: preprocessor._id,
+            courseId: courseId,
+            projectId: projectId
+        }, new InternalProjectRepository())
+    }
+
+    async addProject(courseId: string, projectId: number) {
         const repoProvider = new InternalRepositoryProvider()
         const projectProvider = new InternalProjectRepository()
         const project: Project = await CreateProject(
@@ -36,6 +81,29 @@ export class StudyController {
             courseId: courseId,
             project: project
         }, new InternalCourseRepository());
+        await Promise.all([
+            this.addPreprocessor(courseId, project._id, 'remove-outlier-commits'),
+            this.addPreprocessor(courseId, project._id, 'remove-outlier-blame'),
+            this.addPreprocessor(courseId, project._id, 'remove-outlier-files'),
+            this.addAnalysis(courseId, project._id, 'activity-over-time'),
+            this.addAnalysis(courseId, project._id, 'code-evolution'),
+            this.addAnalysis(courseId, project._id, 'estimated-active-time'),
+            this.addAnalysis(courseId, project._id, 'file-ownership'),
+            this.addAnalysis(courseId, project._id, 'files-by-number-of-coauthors'),
+            this.addAnalysis(courseId, project._id, 'files-by-size'),
+            this.addAnalysis(courseId, project._id, 'files-by-type'),
+            this.addAnalysis(courseId, project._id, 'files-per-commit-number'),
+            this.addAnalysis(courseId, project._id, 'issues-by-completion-time'),
+            this.addAnalysis(courseId, project._id, 'issues-by-member'),
+            this.addAnalysis(courseId, project._id, 'js-code-metrics'),
+            this.addAnalysis(courseId, project._id, 'total-file-ownership'),
+            this.addAnalysis(courseId, project._id, 'typical-commit-size')
+        ])
+
+
+        //
+        //
+        //
     }
 
     async initializeStudySetup(authenticatedUser: any) {
@@ -45,11 +113,11 @@ export class StudyController {
             ownerId: authenticatedUser.githubId,
             authorizeeIds: []
         } as any, authenticatedUser, new InternalCourseRepository());
-        await this.addProject(course._id, '282462669');
-        await this.addProject(course._id, '282264565');
-        await this.addProject(course._id, '282464168');
 
-
+        const ids = [282264565,282462669,282464168,282612137,282612447,282612559,282613443,282620108,282620342,282620687,282620971,282621265,282627029,282627164,282627684,282627780,282630152,282630240]
+        for(let id of ids) {
+            await this.addProject(course._id, id);
+        }
     }
 
     logRegistration(req: express.Request, res: express.Response) {

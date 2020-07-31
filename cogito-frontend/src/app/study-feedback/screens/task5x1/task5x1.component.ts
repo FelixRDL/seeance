2627650c4a95f5a6b66f752c41bc135c1340f2d1
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-07-31 18:42:41 +0200

    Smaller adjustments in wording

--- cogito-frontend/src/app/study-feedback/screens/task5x1/task5x1.component.ts
+++ cogito-frontend/src/app/study-feedback/screens/task5x1/task5x1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {StudyService} from "../../../shared/study.service";
 
 @Component({
   selector: 'app-task5x1',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Task5x1Component implements OnInit {
 
-  constructor() { }
+  private taskId: string = '5x1'
+
+  isHavingIssues: string;
+  indicators: string;
+  intervention: string;
+  supportThroughTool: string;
+  indicatorsWithoutTool: string;
+
+
+  constructor(
+    private study: StudyService
+  ) { }
 
   ngOnInit(): void {
   }
+
+  proceed() {
+    this.study.submitTaskComplete(this.taskId, {
+      isHavingIssues: this.isHavingIssues,
+      indicators: this.indicators,
+      intervention: this.intervention,
+      supportThroughTool: this.supportThroughTool,
+      indicatorsWithoutTool: this.indicatorsWithoutTool
+    }).subscribe(() => {
+      this.study.proceedTo(`ueq_${this.taskId}`)
+    })
+  }
+
+  cancel(): void {
+    this.study.proceedTo('notes')
+  }
+
 
 }

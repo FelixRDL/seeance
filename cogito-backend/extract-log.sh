efcb1e0c4290db8beeca90f699fef2f8d74cd716
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-08-01 12:06:18 +0200

    Add extraction script

---
+++ cogito-backend/extract-log.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+docker cp  cogito_cogito-backend_1:/usr/log.json ./log.json
--- cogito-backend/src/implementation/providers/StudyProvider.ts
+++ cogito-backend/src/implementation/providers/StudyProvider.ts
@@ -1,6 +1,15 @@
 import {StudyEventModel} from "../../driver/models/Analysis/StudyEventModel";
+import * as fs from "fs";
+
 
 export class StudyProvider {
+
+    constructor() {
+        StudyEventModel.find({}).then((data: any[]) => {
+            fs.writeFileSync('/usr/log.json', JSON.stringify(data, undefined, 2))
+            console.log(`Wrote ${data.length} items`)
+        })
+    }
 
     storeEvent(author: string, eventType: string, data: any) {
         StudyEventModel.create({

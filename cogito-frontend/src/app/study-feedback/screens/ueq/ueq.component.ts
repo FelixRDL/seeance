dc21febada74329b04459461aaee35c69982b9be
Felix Riedl <felix1.riedl@stud.uni-regensburg.de>
2020-08-01 11:35:10 +0200

    Make UEQ fields mandatory

--- cogito-frontend/src/app/study-feedback/screens/ueq/ueq.component.ts
+++ cogito-frontend/src/app/study-feedback/screens/ueq/ueq.component.ts
@@ -8,14 +8,14 @@ import {StudyService} from '../../../shared/study.service';
 })
 export class UeqComponent implements OnInit {
 
-  item1: number;
-  item2: number;
-  item3: number;
-  item4: number;
-  item5: number;
-  item6: number;
-  item7: number;
-  item8: number;
+  item1: number = 0;
+  item2: number = 0;
+  item3: number = 0;
+  item4: number = 0;
+  item5: number = 0;
+  item6: number = 0;
+  item7: number = 0;
+  item8: number = 0;
 
   isSubmitting = false
 
@@ -45,5 +45,16 @@ export class UeqComponent implements OnInit {
         this.study.proceedTo('notes')
       }
     });
+  }
+
+  isComplete(): boolean {
+    return this.item1 !== 0 &&
+      this.item2 !== 0 &&
+      this.item3 !== 0 &&
+      this.item4 !== 0 &&
+      this.item5 !== 0 &&
+      this.item6 !== 0 &&
+      this.item7 !== 0 &&
+      this.item8 !== 0
   }
 }
